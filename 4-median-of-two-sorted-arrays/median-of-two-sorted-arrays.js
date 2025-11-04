1580974708b3eddcfd8664ca792bf93d6c58567d
Egor Kotlyar <kotlaregor9@gmail.com>
2025-11-04 21:45:33 +0700

    Time: 4 ms (50.74%) | Memory: 58 MB (80.30%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    const arr = [...nums1, ...nums2].toSorted((a, b) => a - b);
+    if (arr.length % 2 === 0) return (arr[(arr.length / 2) - 1] + arr[arr.length / 2]) / 2;
+    return arr[Math.floor(arr.length / 2)];
+};
