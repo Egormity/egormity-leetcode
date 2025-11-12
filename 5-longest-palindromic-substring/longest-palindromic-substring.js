fb4478e4d9f3c106ceec9250c19422adcf3b5faa
Egor Kotlyar <kotlaregor9@gmail.com>
2025-11-12 18:24:59 +0700

    Time: 44 ms (33.44%) | Memory: 56.3 MB (76.12%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -1,21 +1,33 @@
-const isValidPolindrome = s => {
-    if (s.length === 1) return true;
-    for (let i = 0; i <= s.length / 2; i++) {
-        if (s[i] !== s[s.length - 1 - i]) return false;
-    };
-    return true;
-} 
 /**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function(s) {
-    let longest = s[0];
-    for (let i = 0; i < s.length; i++) {
-        for (let j = i + 1; j < s.length + 1; j++) {
-            const sl = s.slice(i, j);
-            if (sl.length > longest.length && isValidPolindrome(sl)) longest = sl;
-        };
+    // let longest = s[0];
+    // const isValidPolindrome = s => {
+    //     if (s.length === 1) return true;
+    //     for (let i = 0; i <= s.length / 2; i++) {
+    //         if (s[i] !== s[s.length - 1 - i]) return false;
+    //     };
+    //     return true;
+    // } 
+    // for (let i = 0; i < s.length; i++) {
+    //     for (let j = i + 1; j < s.length + 1; j++) {
+    //         const sl = s.slice(i, j);
+    //         if (sl.length > longest.length && isValidPolindrome(sl)) longest = sl;
+    //     };
+    // };
+    let longest = [0, 0];
+    const helper = (l, r) => {
+        while (s[l] === s[r] && l >= 0 && r < s.length) {
+        if (r - l > longest[1] - longest[0]) longest = [l, r];
+        l--;
+        r++;
+        }
     };
-    return longest;
+    for (let i = 0; i < s.length; i++) {
+        helper(i - 1, i + 1);
+        helper(i, i + 1);
+    }
+    return s.slice(longest[0], longest[1] + 1);
 };
