08ce0711e6f941118ea6a4f8ba25376e5129b6a1
Egor Kotlyar <kotlaregor9@gmail.com>
2025-11-13 11:44:26 +0700

    Time: 54 ms (59.39%) | Memory: 55.6 MB (60.00%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -4,16 +4,16 @@
  */
 const max = 2 ** 31;
 var reverse = function(x) {
-    // const isNegative = x < 0;
-    // const result = +(x + "").slice(isNegative ? 1 : 0).split("").reverse().join("") * (isNegative ? -1 : 1);
-    // if (result < -max || result > max + 1) return 0;
-    // return result;
-    let result = 0;
-    while (x !== 0) {
-        digit = x % 10;
-        result = result * 10 + digit;
-        x = Math.trunc(x / 10);
-    };
+    const isNegative = x < 0;
+    const result = +(x + "").slice(isNegative ? 1 : 0).split("").reverse().join("") * (isNegative ? -1 : 1);
     if (result < -max || result > max + 1) return 0;
     return result;
+    // let result = 0;
+    // while (x !== 0) {
+    //     digit = x % 10;
+    //     result = result * 10 + digit;
+    //     x = Math.trunc(x / 10);
+    // };
+    // if (result < -max || result > max + 1) return 0;
+    // return result;
 };
