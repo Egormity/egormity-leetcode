7394e1d5e2f384338e8377f422e89c5a6ecdf3bf
Egor Kotlyar <kotlaregor9@gmail.com>
2025-11-13 11:47:24 +0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (88.42%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -1,23 +1,3 @@
-// /**
-//  * @param {number} x
-//  * @return {number}
-//  */
-// const max = 2 ** 31;
-// var reverse = function(x) {
-//     // const isNegative = x < 0;
-//     // const result = +(x + "").slice(isNegative ? 1 : 0).split("").reverse().join("") * (isNegative ? -1 : 1);
-//     // if (result < -max || result > max + 1) return 0;
-//     // return result;
-//     let result = 0;
-//     while (x !== 0) {
-//         digit = x % 10;
-//         result = result * 10 + digit;
-//         if (result < -max || result > max + 1) return 0;
-//         x = Math.trunc(x / 10);
-//     };
-//     return result;
-// };
-
 /**
  * @param {number} x
  * @return {number}
@@ -46,3 +26,22 @@ else{
 process.on("exit", () => {
     require("fs").writeFileSync("display_runtime.txt", "0");
 })
+// /**
+//  * @param {number} x
+//  * @return {number}
+//  */
+// const max = 2 ** 31;
+// var reverse = function(x) {
+//     // const isNegative = x < 0;
+//     // const result = +(x + "").slice(isNegative ? 1 : 0).split("").reverse().join("") * (isNegative ? -1 : 1);
+//     // if (result < -max || result > max + 1) return 0;
+//     // return result;
+//     let result = 0;
+//     while (x !== 0) {
+//         digit = x % 10;
+//         result = result * 10 + digit;
+//         if (result < -max || result > max + 1) return 0;
+//         x = Math.trunc(x / 10);
+//     };
+//     return result;
+// };
