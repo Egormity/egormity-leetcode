b080757ba2faa3601ff4ba809adfff25aa0e6b49
Egor Kotlyar <kotlaregor9@gmail.com>
2025-11-14 14:34:57 +0700

    Time: 7 ms (59.02%) | Memory: 65.5 MB (46.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.ts
+++ 9-palindrome-number/palindrome-number.ts
@@ -1,3 +1,9 @@
 function isPalindrome(x: number): boolean {
+    if (x < 0) return false;
     return (x + "").split("").reverse().join("") === x + "";
+    // let reversed = 0;
+    // while (x !== 0) {
+
+    // };
+    // return x === reversed
 };
